3135a06eef47eee5362579749a257a6667478a7c
Segun228 <segunperkele@gmail.com>
2025-04-13 20:48:56 +0300

    adding mobile controls

--- src/components/control/Control.jsx
+++ src/components/control/Control.jsx
@@ -1,13 +1,29 @@
 import "./control.css"
+import { useDispatch } from "react-redux";
+import { saveKey } from "../../store/gameSlice";
 const Control = () => {
+    const dispatch = useDispatch();
+    const clickUp = ()=>{
+        dispatch(saveKey("w"));
+    }
+    const clickRight = ()=>{
+        dispatch(saveKey("d"));
+    }
+    const clickDown = ()=>{
+        dispatch(saveKey("s"));
+    }
+    const clickLeft = ()=>{
+        dispatch(saveKey("a"));
+    }
+
     return (
     <div className="control-wrapper">
-        <button className="control_button control_button_up"></button>
+        <button className="control_button control_button_up" onClick={clickUp}></button>
         <div className="horizontal__container">
-            <button className="control_button control_button_left"></button>
-            <button className="control_button control_button_right"></button>
+            <button className="control_button control_button_left" onClick={clickLeft}></button>
+            <button className="control_button control_button_right" onClick={clickRight}></button>
         </div>
-        <button className="control_button control_button_down"></button>
+        <button className="control_button control_button_down" onClick={clickDown}></button>
     </div>);
 }
 
